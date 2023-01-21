2bdf06324114d70e90c91d10954d3109ba025a40
UnnamedMistress <momchrysti@gmail.com>
2023-01-21 13:42:41 -0500

    working readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,56 +1,89 @@
-"type";"module"
+//
 // packages needed for this application
 //package needed: npm inquirer JSON package
-
-const inquirer = require('inquirer');
-//  array of questions for user input
-const questions = ['What was your motivation', "Why did you build this project?", "What problem does it solve", "What did you learn?", "What makes your project stand out?", "What installation did you use (inquirer)", "What license does this have?", "Who gets the credit (you)", "Steps required to install the project?", "Intended usage"];
-
-//  a function to write README file
 const fs = require('fs');
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, function(err) {
-        if(err) {
-            return console.log(err);
-        }
-        console.log("The file was saved!");
-    }); 
-}
+const inquirer = require('inquirer');
+const {renderLicenseBadge} = require('./generateMarkdown.js');
+const { renderLicenseLink} = require('./generateMarkdown.js');
 
-// a function to initialize app
+//  array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    message: 'Why did you build this project?',
+    name: 'reasonForBuild',
+    
+  },
+  {
+    type: 'input',
+    message: 'What problem does it solve?',
+      name: 'problemSolved'
+  },
+  {
+    type: 'input',
+    message: 'What installation did you use (inquirer)',
+    name: 'installation',
+    
+  },
+  {
+    type: 'list',
+    message: 'What license does this have?',
+    name: 'license',
+    choices: ['MIT', 'ISC', 'GPL'],
+    
+  },
+  {
+    type: 'input',
+     message: 'Who gets the credit (you)',
+    name: 'credit',
+   
+  },
+  {
+    type: 'input',
+    message: 'Steps required to install the project?',
+    name: 'installationSteps',
+    
+  },
+  {
+    type: 'input',
+    message: 'Intended usage',
+    name: 'intendedUsage',
+    
+  },
+];
+
+
 function init() {
-    inquirer
-    .prompt(questions)
-    .then((answers) => {
-      // Use the answers to generate the README file
-      const data = generateReadmeData(answers);
-      writeToFile("README.md", data);
-    })
-    .catch((error) => {
-      if (error.isTtyError) {
-        // Prompt couldn't be rendered in the current environment
-      } else {
-        // Something else went wrong
-      }
+  
+inquirer
+.prompt(questions)
+   .then((answers) => {
+    const filename = `${answers.reasonForBuild.toLowerCase().split(' ').join('')}.json`;
+    fs.writeFile(filename, JSON.stringify(questions && answers, null, '\t'), (err) => {
+        if (err) throw err;
+        console.log(answers);
+        fs.readFile(filename, (err, data) => {
+          if (err) throw err;
+          const jsonData = JSON.parse(data);
+          const licenseBadge = renderLicenseBadge(jsonData.license);
+          const link = renderLicenseLink(jsonData.license);
+          let readme = `
+          # Title: ${jsonData.reasonForBuild}\n\n
+          ## Problem Solved \n${jsonData.problemSolved}\n\n
+          ## Installation \n${jsonData.installation}\n\n
+          ## License and Link: ${link}\n${licenseBadge}\n\n
+          ## Credit \n${jsonData.credit}\n\n
+          ## Installation Steps \n${jsonData.installationSteps}\n\n
+          ## Intended Usage \n${jsonData.intendedUsage}`;
+          // Append the badge to the readme file
+          fs.writeFile("readme.md", readme, (err) => {
+              if (err) throw err;
+              console.log("readme.md file created");});
+
+          })
     });
-}
-
-
+  });}
 // Function call to initialize app
 init();
 
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-    console.log(questions)
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+
---
+++ Develop/readme.md
@@ -0,0 +1,25 @@
+
+          # Title: 
+
+
+          ## Problem Solved 
+
+
+
+          ## Installation 
+
+
+
+          ## License and Link: [MIT](https://opensource.org/licenses/MIT/)
+[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/licenses/MIT)
+
+
+          ## Credit 
+
+
+
+          ## Installation Steps 
+
+
+
+          ## Intended Usage 
